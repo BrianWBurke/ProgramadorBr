04de0578e65b546ead38ddd7992ebcb3f6744af7
Brian <brianetec@gmail.com>
2022-06-16 11:37:02 -0300

    clear

---
+++ Modulo 09/temporizadores/ClearTimeout/script.js
@@ -0,0 +1,21 @@
+var numero = 0;
+
+var timer;
+
+function atualizarTexto() {
+    let h = document.getElementsByTagName('h1')[0];
+
+    h.innerHTML += ' ' + numero;
+
+    numero++;
+}
+
+
+function iniciar() {
+    timer = setTimeout(atualizarTexto, 3000);
+}
+
+function parar() {
+    clearInterval(timer);
+}
+

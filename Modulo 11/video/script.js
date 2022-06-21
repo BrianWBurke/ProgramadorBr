2835416ca53fed0ec7817870fc98aa995e258196
Brian <brianetec@gmail.com>
2022-06-21 16:18:48 -0300

    commit

--- Modulo 11/video/script.js
+++ Modulo 11/video/script.js
@@ -1,32 +1,6 @@
 
 
 let video = document.getElementById('video1');
-// let container = document.getElementsByClassName('container');
-
-
-function ocultar() {
-    let controle = document.getElementsByClassName('controle').display;
-    controle = 'inline';
-}
-
-// container.addEventListener('mouseout', function(){
-//     let controle = document.getElementsByClassName('controle');
-//     if(controle.display === 'none') {
-//         controle.display = 'inline';
-//     } else {
-//         controle.display = 'none';
-//     }
-// });
-
-
-// function ocultar() {
-//     let img = document.getElementsByClassName('controle');
-//     if (img.visibility === 'hidden'){
-//         img.visibility = 'visible';        
-//     } else { 
-//         img.visibility = 'hidden';
-//     }
-// }
 
 function retroceder() {
     video.currentTime -= 15;

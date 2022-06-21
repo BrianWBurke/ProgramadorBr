55ab8fdf04f767bc99c3d684d7092864697d370c
Brian <brianetec@gmail.com>
2022-06-21 16:34:17 -0300

    commit

--- Modulo 10/Churrascômetro ProgBr/script.js
+++ Modulo 10/Churrascômetro ProgBr/script.js
@@ -4,11 +4,14 @@ let inputDuracao = document.getElementById('duracao');
 
 let resultado = document.getElementById('resultado');
 
-function calcular(pullback) {
+function calcular(ret) {    
     let adultos = inputAdultos.value;
     let criancas = inputCriancas.value;
     let duracao = inputDuracao.value;
 
+    if (ret == true) {
+        alert('Por favor, preencha todos os camps')
+    } else {
     let qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas);
     let qdtTotalCerveja = cervejaPP(duracao) * adultos;
     let qdtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas);
@@ -18,9 +21,15 @@ function calcular(pullback) {
     resultado.innerHTML += `<p>${Math.ceil(qdtTotalCerveja / 355)} Latas (355ml cada) de Cerveja &#x1F37A</p>`
     resultado.innerHTML += `<p>${Math.ceil(qdtTotalBebidas / 2000)} Garrafas (2L) de Bebida &#x1F964</p>`
 }
+}
 
-function verificar() {
-    
+function verificar(callback) {
+    let ret;
+    if (document.getElementById('adultos') == '' ||  document.getElementById('criancas') == '' || document.getElementById('duracao') == '') {
+        ret = true;
+    } else {
+        ret = false;
+    }
 }
 
 function carnePP(duracao) {    

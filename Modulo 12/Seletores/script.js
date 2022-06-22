ff37584df302af367b486cdd858990901eddabef
Brian <102098077+BrianWBurke@users.noreply.github.com>
2022-06-22 11:33:04 -0300

    seletores

---
+++ Modulo 12/Seletores/script.js
@@ -0,0 +1,16 @@
+// também podemos utilizar o title para pegar os elementos
+// pegando o primeiro elemento:
+// let titulos = document.querySelector('[title]');
+
+// console.log(titulos);
+
+
+// pegando todos os elementos:
+// let titulos = document.querySelectorAll('[title]');
+
+// console.log(titulos);
+
+//outro exemplo:
+// let titulos = document.querySelectorAll('[title*=tu]');
+
+// console.log(titulos);

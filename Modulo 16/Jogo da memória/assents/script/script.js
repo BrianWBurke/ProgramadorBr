5744419fe1658c6480a462df6bff1d8ae9c8650f
Brian <102098077+BrianWBurke@users.noreply.github.com>
2022-07-14 12:43:17 -0300

    att img's e flip

--- Modulo 16/Jogo da memória/assents/script/script.js
+++ Modulo 16/Jogo da memória/assents/script/script.js
@@ -1,5 +1,7 @@
 const FRONT = "card_front";
 const BACK = "card_back";
+const CARD = "card";
+const ICON = "icon";
 
 let techs = ['bootstrap',
     'css',
@@ -18,8 +20,44 @@ let techs = ['bootstrap',
 
     function startGame() {
         cards = createCardsFromTechs(techs);
-        shuffleCards(cards);
-        console.log(cards);
+        shuffleCards(cards);        
+        initializeCards(cards);
+    }
+    
+    function initializeCards(cards) {
+        let gameBoard = document.querySelector('#gameBoard');
+        
+        cards.forEach(card => {
+            let cardElement = document.createElement('div');
+            cardElement.id = card.id;
+            cardElement.classList.add(CARD);
+            cardElement.dataset.icon = card.icon;
+
+            createCardContent(card, cardElement);
+
+            cardElement.addEventListener('click', flipCard)
+            gameBoard.appendChild(cardElement);
+        })
+
+    }
+
+    function createCardContent(card, cardElement) {
+        createCardFace(FRONT, card, cardElement);
+        createCardFace(BACK, card, cardElement);
+    }
+
+    function createCardFace(face, card, element) {
+        let cardElementFace = document.createElement('div');
+        cardElementFace.classList.add(face);
+        if(face === FRONT) {
+            let iconElement = document.createElement('img');
+            iconElement.classList.add(ICON)
+            iconElement.src = './assents/images/' + card.icon + ".png"
+            cardElementFace.appendChild(iconElement);
+        } else {
+            cardElementFace.innerHTML = '&lt/&gt';
+        }
+        element.appendChild(cardElementFace);
     }
 
     function shuffleCards(cards) {
@@ -38,9 +76,10 @@ let techs = ['bootstrap',
     function createCardsFromTechs(techs) {
         let cards = [];
 
-        for(let tech of techs) {
+
+        techs.forEach((tech) => {
             cards.push(createPairFromTech(tech));
-        }
+        })
 
         return cards.flatMap(pair => pair);
     }
@@ -61,4 +100,8 @@ let techs = ['bootstrap',
 
     function createIdWithTech(tech) {
         return tech + parseInt(Math.random() * 1000);
+    }
+
+    function flipCard() {
+        this.classList.add('flip');
     }

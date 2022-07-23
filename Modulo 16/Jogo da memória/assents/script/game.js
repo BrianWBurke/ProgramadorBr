e0290697c7adaa0540788c010315921f1ff83dd5
Brian <102098077+BrianWBurke@users.noreply.github.com>
2022-07-23 16:47:51 -0300

    corrigindo bugs

--- Modulo 16/Jogo da memória/assents/script/game.js
+++ Modulo 16/Jogo da memória/assents/script/game.js
@@ -4,36 +4,47 @@ let game = {
     firstCard: null,
     secondCard: null,
 
-    setCard:function(id) {
+    setCard: function (id) {
 
-        let card = this.cards.filter(card=>card.id==id)[0];
+        let card = this.cards.filter(card => card.id == id)[0];
 
         if (card.flipped || this.lockMode) {
             return false;
         }
 
-        if(!this.firstCard) {
+        if (!this.firstCard) {
             this.firstCard = card;
+            this.firstCard.flipped = true;
             return true;
         } else {
             this.secondCard = card;
+            this.secondCard.flipped = true;
             this.lockMode = true;
             return true;
         }
 
     },
 
-    checkMatch:function() {
+    checkMatch: function () {
+        if (!this.firstCard || !this.secondCard) {
+            return false;
+        }
         return this.firstCard.icon === this.secondCard.icon;
     },
 
-    clearCards: function() {
+    clearCards: function () {
         this.firstCard = null;
         this.secondCard = null;
         this.lockMode = false;
     },
 
-    techs : ['bootstrap',
+    unflipCards() {
+        this.firstCard.flipped = false;
+        this.secondCard.flipped = false;
+        this.clearCards()
+    },
+
+    techs: ['bootstrap',
         'css',
         'electron',
         'firebase',
@@ -44,7 +55,7 @@ let game = {
         'node',
         'react'],
 
-    cards : null,
+    cards: null,
 
     createCardsFromTechs: function () {
         this.cards = [];
@@ -55,8 +66,8 @@ let game = {
         })
 
         this.cards = this.cards.flatMap(pair => pair);
-        this.shuffleCards();  
-        return this.cards;        
+        this.shuffleCards();
+        return this.cards;
     },
 
     createPairFromTech: function (tech) {
@@ -81,12 +92,12 @@ let game = {
         let currentIndex = this.cards.length;
         let randomIndex = 0;
 
-        while(currentIndex !== 0) {
+        while (currentIndex !== 0) {
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex--;
 
             [this.cards[randomIndex], this.cards[currentIndex]] = [this.cards[currentIndex], this.cards[randomIndex]];
         }
-    }   
-    
+    }
+
 }
